6859f1c2c538c2dce3c6b2cc244757d8427a5255
hungle8795 <quanghung8795@gmail.com>
2024-07-11 17:02:45 +0900

    Release 20240711.1

---
+++ Net-React.Server/Models/ProductDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Net_React.Server.Models;
+
+public partial class ProductDetail
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Quantity { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual Category Category { get; set; } = null!;
+}
