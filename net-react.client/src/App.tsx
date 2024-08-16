a05bc81baf7161618773fbdec6e7adcca6d809c8
hungle8795 <quanghung8795@gmail.com>
2024-08-16 17:49:28 +0900

    Release 20240816.2
    Create header and add japanese font

--- net-react.client/src/App.tsx
+++ net-react.client/src/App.tsx
@@ -6,6 +6,7 @@ import DeleteCategory from './DeleteCategory';
 import React from 'react';
 import UpdateCategory from './UpdateCategory';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //interface Forecast {
 //    date: string;
--- net-react.client/src/components/Header.tsx
+++ net-react.client/src/components/Header.tsx
@@ -14,41 +14,43 @@ const Header: React.FC = () => {
 
             <div
                 className="navbar navbar-expand-sm bg-dark navbar-dark nav-pills fixed-top justify-content-between pl-5 pt-3 pr-5 pb-3">
-                <a className="navbar-logo" href="#logo" id="#logo">
-                    <img src="../img/logo-dark.png" />
-                </a>
+                {/*<a className="navbar-logo" href="#logo" id="#logo">*/}
+                {/*    <img src="../img/logo-dark.png" />*/}
+                {/*</a>*/}
+                <h4 className="text-white">ホームページ</h4>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
                     <ul className="navbar-nav align-item-center">
+                        {/*<li className="nav-item">*/}
+                        {/*    <a className="nav-link mb-0 mr-3" href="#home">HOME</a>*/}
+                        {/*</li>*/}
+                        {/*<li className="nav-item">*/}
+                        {/*    <a className="nav-link mb-0 mr-3" href="#features">FEATURES</a>*/}
+                        {/*</li>*/}
+                        {/*<li className="nav-item">*/}
+                        {/*    <a className="nav-link mb-0 mr-3" href="#portfolio">PORTFOLIO</a>*/}
+                        {/*</li>*/}
+                        {/*<li className="nav-item">*/}
+                        {/*    <a className="nav-link mb-0 mr-3" href="#resume">RESUME</a>*/}
+                        {/*</li>*/}
+                        {/*<li className="nav-item">*/}
+                        {/*    <a className="nav-link mb-0 mr-3" href="#clients">CLIENTS</a>*/}
+                        {/*</li>*/}
                         <li className="nav-item">
-                            <a className="nav-link mb-0 mr-3" href="#home">HOME</a>
+                            <form className="nav-link mb-0 mr-3" >
+                                <input placeholder="検索" className="text-left">
+                                    {/*<p className="text-right">tim</p>*/}
+                                </input>
+                            </form>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link mb-0 mr-3" href="#features">FEATURES</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link mb-0 mr-3" href="#portfolio">PORTFOLIO</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link mb-0 mr-3" href="#resume">RESUME</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link mb-0 mr-3" href="#clients">CLIENTS</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link mb-0 mr-3" href="#pricing">PRICING</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link mb-0 mr-3" href="#blog">BLOG</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link mb-0 mr-3" href="#contact">CONTACT</a>
+                            <a className="nav-link mb-0 mr-3" href="#pricing">サインアップ</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="https://themeforest.net/checkout/82475332/create_account">
-                                <button className="btn bg-white text-danger">BUY NOW</button>
+                                <button className="btn bg-white text-danger">ログイン</button>
                             </a>
                         </li>
                     </ul>
